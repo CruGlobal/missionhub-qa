6d0bde7333bdfb7da025ed0522ace5b18498e3d3
Bob Buford <bobbuford@C02XQC5KJG5J.local>
2019-06-11 12:13:54 -0400

    remove test result files and amend API key

--- missionhub-apk/src/main/java/missionhub/com/Helper/Config.java
+++ missionhub-apk/src/main/java/missionhub/com/Helper/Config.java
@@ -3,8 +3,8 @@ package missionhub.com.Helper;
 public class Config {
     private static String KOBITON_CREDENTIAL = "<API_KEY>@api.kobiton.com/wd/hub";
     private static String APP_URL="https://s3-ap-southeast-1.amazonaws.com/kobiton-devvn/asg_apps/MissionHub.apk";
-    private static String APP_USERNAME = "test@test.com";
-    private static String APP_PASSWORD = "Test1234";
+    private static String APP_USERNAME = TEST_USER;
+    private static String APP_PASSWORD = TEST_PWD;
 
     public static String getCredential() {
         return KOBITON_CREDENTIAL;
