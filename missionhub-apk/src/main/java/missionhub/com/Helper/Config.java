e0213dc57dd755083098777a0967f5302892f049
Bob Buford <bobbuford@C02XQC5KJG5J.local>
2019-06-11 12:08:17 -0400

    obfuscated API Key

--- missionhub-apk/src/main/java/missionhub/com/Helper/Config.java
+++ missionhub-apk/src/main/java/missionhub/com/Helper/Config.java
@@ -1,8 +1,7 @@
 package missionhub.com.Helper;
 
 public class Config {
-    private static String KOBITON_CREDENTIAL = "https://bufordr1:84fd0f29-23e8-47f4-8ba2-223f737ba768@api.kobiton.com/wd/hub";  //Bob Buford login
-//    private static String KOBITON_CREDENTIAL = "https://Justin_Custer:a0fe65f9-1e46-44f1-85fa-ee24bbccb004@api.kobiton.com/wd/hub";   //Justin Custer login
+    private static String KOBITON_CREDENTIAL = "<API_KEY>/wd/hub";
     private static String APP_URL="https://s3-ap-southeast-1.amazonaws.com/kobiton-devvn/asg_apps/MissionHub.apk";
     private static String APP_USERNAME = "test@test.com";
     private static String APP_PASSWORD = "Test1234";
